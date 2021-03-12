eb4e1e87760e1ee2d4a7d0f74d11abb86e1b2d01
androosk <atirpok3@gmail.com>
2021-03-11 22:47:36 -0700

    start 1.3 refactor 1.2

---
+++ longest-substring-with-k-distinct-characters.js
@@ -0,0 +1,11 @@
+function myFunction(str, k) {
+  const strArr = str.split('');
+
+  for (let i=0; i<strArr.length; i++) {
+
+  }
+
+  console.log(strArr)
+}
+
+myFunction("araaci", 2) // expected output: 4
--- smallest-subarray-with-a-given-sum-easy.js
+++ smallest-subarray-with-a-given-sum-easy.js
@@ -1,26 +1,20 @@
 function myFunction(s, arr) {
-  let result = 0;
-  let sum = 0, subLength = 0, lengths = [];
+  let sum = 0, length = Infinity, start =0;
 
-  if (Math.max(...arr)>=s) { return 1 } else {
-    for(let i=0; i<arr.length; i++) {
-      subLength++
-      sum += arr[i]
-      if(sum >= s) {
-        lengths.push(subLength)
-        subLength--
-        sum-=arr[i-subLength]
-      }
-      if(sum >= s) {
-        lengths.push(subLength)
-        subLength--
-        sum-=arr[i-subLength]
-      }
+  for (let i=0; i < arr.length; i++) {
+    sum += arr[i];
+
+    while (sum >=s) {
+      length = Math.min(length, i-start+1);
+      sum -= arr[start];
+      start += 1;
     }
-    return Math.min(...lengths);
   }
-
+  if (length === Infinity) {
+    console.log(0);
+  }
+  console.log(length);
 }
 
 
-myFunction (8,[3,4,1,1,6])
+myFunction (7, [2, 1, 5, 2, 8])
