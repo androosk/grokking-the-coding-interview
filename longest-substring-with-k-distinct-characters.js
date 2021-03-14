efdcb936b95fe28afad0d3f3530221d4437947c5
androosk <atirpok3@gmail.com>
2021-03-14 14:12:20 -0600

    longest substring

--- longest-substring-with-k-distinct-characters.js
+++ longest-substring-with-k-distinct-characters.js
@@ -1,11 +1,30 @@
+
 function myFunction(str, k) {
-  const strArr = str.split('');
+  let start = 0;
+  let length = 0;
+  let freqObj = {};
 
-  for (let i=0; i<strArr.length; i++) {
-
+  for (let i=0; i <str.length; i++){
+    let right = str[i]
+    if(!(right in freqObj)) {
+      freqObj[right] = 0
+      console.log('Added ', right)
+    }
+    freqObj[right] +=1
+    while (Object.keys(freqObj).length > k) {
+      let left = str[start]
+      freqObj[left] -=1
+      if (freqObj[left] === 0) {
+        console.log('removing ', left)
+        delete freqObj[left]
+      }
+    start += 1;
   }
-
-  console.log(strArr)
+    length = Math.max(length, i - start +1)
+  }
+  console.log('Longest Substring Length: ', length)
 }
 
 myFunction("araaci", 2) // expected output: 4
+myFunction("araaci", 1) // expected output: 2
+myFunction("cbbebi", 3) // expected output: 5
